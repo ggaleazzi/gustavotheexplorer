32948e4bdd4864b8ea3b1f94ff1fe85eccf4f71e
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2022-03-22 15:47:19 -0500

    fix 33

--- wedding/index.js
+++ wedding/index.js
@@ -15,10 +15,15 @@ function initMap() {
   }
 
   $("video").prop('muted', true);
-    $("#mute-video").click( function (){
-      if( $("video").prop('muted') ) {
-            $("video").prop('muted', false);
+
+  $(".mute-video").click(function () {
+      if ($("video").prop('muted')) {
+          $("video").prop('muted', false);
+          $(this).addClass('unmute-video'); // changing icon for button
+  
       } else {
-        $("video").prop('muted', true);
+          $("video").prop('muted', true);
+          $(this).removeClass('unmute-video'); // changing icon for button
       }
+      console.log($("video").prop('muted'))
   });
