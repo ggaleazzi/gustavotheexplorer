5db4a078faf149d726e0c63a1acb8423cf63c8c1
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2022-03-22 15:54:07 -0500

    fix 36

--- wedding/index.js
+++ wedding/index.js
@@ -1,18 +1,4 @@
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const Finca = { lat: 4.929324264187976, lng: -74.13521732188201 };
-    // The map, centered at Finca
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 13,
-      center: Finca,
-    });
-    // The marker, positioned at Finca
-    const marker = new google.maps.Marker({
-      position: Finca,
-      map: map,
-    });
-  }
+
 
   $(document).ready(function () {
     $("video").prop('muted', true);
