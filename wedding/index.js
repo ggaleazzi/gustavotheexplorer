cdfe9a8ff7bfa4509a2f9f942c0e9e20ef66cf58
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2022-03-22 13:24:47 -0500

    fix 24

--- wedding/index.js
+++ wedding/index.js
@@ -13,4 +13,48 @@ function initMap() {
       map: map,
     });
   }
+
+
+  
+
+    function toggleMute() 
+
+    {
+
+        var video = document.getElementById("myvideo");
+
+        if (video.muted) 
+
+        {
+
+            video.muted = false;
+
+        } 
+
+        else 
+
+        {
+
+            debugger;
+
+            video.muted = true;
+
+            video.play()
+
+        }
+
+    }
+
+    $(document).ready(function () 
+
+    {
+
+        setTimeout(toggleMute, 3000);
+
+    }
+
+    )
+
+
+
   
