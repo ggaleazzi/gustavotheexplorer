ed82ac326bfdd3247337dbabb889bb03c4f06930
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2022-03-30 22:02:55 -0500

    fix 114

--- wedding/map.js
+++ wedding/map.js
@@ -8,36 +8,5 @@ L.tileLayer(tilesProvider, {
 
 let marker = L.marker([4.929324264187976, -74.13521732188201]).addTo(myMap)
 
-let iconMarker = L.icon({
-    iconUrl: 'marker.png',
-    iconSize: [60, 60],
-    iconAnchor: [30, 60]
-})
 
-let marker2 = L.marker([4.929324264187976, -74.13521732188201], { icon: iconMarker }).addTo(myMap)
 
-myMap.doubleClickZoom.disable()
-myMap.on('dblclick', e => {
-  let latLng = myMap.mouseEventToLatLng(e.originalEvent);
-
-  L.marker([latLng.lat, latLng.lng], { icon: iconMarker }).addTo(myMap)
-})
-
-navigator.geolocation.getCurrentPosition(
-  (pos) => {
-    const { coords } = pos
-    const { latitude, longitude } = coords
-    L.marker([latitude, longitude], { icon: iconMarker }).addTo(myMap)
-
-    setTimeout(() => {
-      myMap.panTo(new L.LatLng(latitude, longitude))
-    }, 5000)
-  },
-  (error) => {
-    console.log(error)
-  },
-  {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  })
