dcbe80f3d035010b761261cdcd19aaf0bf289a16
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2022-03-30 22:04:20 -0500

    fix 115

--- wedding/map.js
+++ wedding/map.js
@@ -1,6 +1,6 @@
 const tilesProvider = `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`
 
-let myMap = L.map('myMap').setView([4.92, -74.13], 13)
+let myMap = L.map('myMap').setView([4.92, -74.13], 10)
 
 L.tileLayer(tilesProvider, {
 	maxZoom: 18,
